17f09ee74ee6c7cf5c8f69a5559296ffd3d92311
stefenforli <380031499@qq.com>
2016-10-18 14:24:28 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -6,6 +6,7 @@ import httplib
 import urlparse
 import base64
 import json
+import pymongo
 
 def getStocks(url, useproxy = False, proxy_url=''):
     data = ''
@@ -35,28 +36,40 @@ def writeStock(stocks, filename):
     fi.write(stocks)
     fi.close()
 
+def writeStock2mongo(stocks, client, dbname='test', collectionname='stockstest'):
+    db=client[dbname]
+    collection = db[collectionname]
+    for everystock in stocks:
+        collection.insert_one(everystock)
+    return True
+	
+	
 def readStock(stocks, filename):
     """@stocks: return as json"""
     """struct of stocks:
-       [{stock_number1, stock_name1}, {stock_number2, stock_name2},...]"""
+    [{stock_number1,stock_name1}, {stock_number2,stock_name2},...]"""
     fi = file(filename, 'r')
     stocks = fi.read()
     fi.close()
-    stocks = stocks[0:-1]
+    stocks = json.loads(stocks)
     return stocks
 
 if __name__=="__main__":
 	
+    """here for test code"""
     #start time
     starttime = time.clock()
 
-    SHAurl = 'http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx?type=FD&sty=TSTC&st=1&sr=1&p=1&ps=2000&js=var%20GQWitEGZ=(x)&mkt=1'
-    SZAurl = 'http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx?type=FD&sty=TSTC&st=1&sr=1&p=1&ps=2000&js=var%20mhKpHKDS=(x)&mkt=3'
-
+    #SHAurl = 'http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx?type=FD&sty=TSTC&st=1&sr=1&p=1&ps=2000&js=var%20GQWitEGZ=(x)&mkt=1'
+    #SZAurl = 'http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx?type=FD&sty=TSTC&st=1&sr=1&p=1&ps=2000&js=var%20mhKpHKDS=(x)&mkt=3'
+    
+    stocks = readStock('d:/__P/test-master/SZA.stk')
+    client=pymongo.MongoClient()
+    writeStock2mongo(stocks, client)
 
     #allstocks = getStocks(SHAurl)
-    writeStock(getStocks(SHAurl), '/Users/stefen/Desktop/stock/SHA.stk')
-    writeStock(getStocks(SZAurl), '/Users/stefen/Desktop/stock/SZA.stk')
+    #writeStock(getStocks(SHAurl), '/Users/stefen/Desktop/stock/SHA.stk')
+    #writeStock(getStocks(SZAurl), '/Users/stefen/Desktop/stock/SZA.stk')
 
     #end time
     endtime = time.clock()
